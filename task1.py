52171f6660bbb6744abe3aa2246daeb54d34de93
Nek1t_ <144207428+Nek1tt@users.noreply.github.com>
2025-09-24 20:13:48 +0700

    Delete task1.py

--- task1.py
+++
@@ -1,7 +0,0 @@
-import gensim.downloader as api
-
-model = api.load("word2vec-google-news-300") # загрузка модели (шаг 1)
-
-print(model.most_similar('king')) # вывод синонимов для слова king
- 
-print(model.most_similar(positive=['king', 'woman'], negative=['man'])) # вывод аналогии для king - man + woman
